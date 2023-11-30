ddf35b4ca5f6da7ffcefc7c6bdb29e5cf061daa8
louis <ws.choi@anypointmedia.com>
2023-11-30 11:40:50 +0900

    this expressions

--- src/main/kotlin/ThisExpressions.kt
+++ src/main/kotlin/ThisExpressions.kt
@@ -1,2 +1,43 @@
+class A { // implicit labl @A
+    inner class B { // implicit label @B
+        fun Int.foo() { // implicit label @foo
+            val a = this@A // A's this
+            val b = this@B // B's this
+
+            val c = this // foo()'s receiver, an Int
+            val c1 = this@foo // foo()'s receiver, an Int
+
+            val funLit = lambda@ fun String.() {
+                val d = this // funLit's receiver, a String
+            }
+
+            val funLit3 = fun String.() {
+                val d = this // funLit3's receiver, a String
+            }
+
+            val funLit2 = { s: String ->
+                // foo()'s receiver, since enclosing lambda expression
+                // doesn't have any receiver
+                val d1 = this
+            }
+        }
+    }
+}
+
+
 fun main() {
+
+    fun printLine() { println("Top-level function") }
+
+    class A {
+        fun printLine() { println("Member function") }
+
+        fun invokePrintLine(omitThis: Boolean = false)  {
+            if (omitThis) printLine()
+            else this.printLine()
+        }
+    }
+
+    A().invokePrintLine() // Member function
+    A().invokePrintLine(omitThis = true) // Top-level function
 }
