470d8cbc7476c49ed7074e3b98054a39591b26cf
louis <ws.choi@anypointmedia.com>
2023-12-01 16:58:22 +0900

    Sequences.kt

--- src/main/kotlin/Sequences.kt
+++ src/main/kotlin/Sequences.kt
@@ -1,3 +1,5 @@
+import kotlin.random.Random
+
 fun main() {
     val list = listOf(1, 2, 3, 4, 5)
 
@@ -6,4 +8,9 @@ fun main() {
     println("\n------")
     list.asSequence().onEach { print("$it ") }.filter { it % 2 == 0 }.forEach { print("$it ") }
     // output: 1 2 2 3 4 4 5
+
+    println("\n------")
+
+//    generateSequence(0) { it + 1 }.shuffled().take(5).forEach { print("$it ") } // infinite because shuffled is terminal
+    generateSequence { Random.nextInt() }.distinct().take(5).forEach { print("$it ") }
 }
