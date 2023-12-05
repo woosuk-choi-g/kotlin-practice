3d6f13faa00218c3eaf7406d82474b4bbad1af0e
louis <ws.choi@anypointmedia.com>
2023-12-05 12:42:23 +0900

    Deferred.kt

---
+++ src/main/kotlin/Defered.kt
@@ -0,0 +1,16 @@
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+
+fun main() = runBlocking {
+    val deferreds = (1..3).map {
+        async {
+            delay(1000L * it)
+            println("Loading $it")
+            it
+        }
+    }
+    val sum = deferreds.awaitAll().sum()
+    println("$sum")
+}
