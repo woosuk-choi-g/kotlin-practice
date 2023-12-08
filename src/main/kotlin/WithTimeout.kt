e9f29d6f4a68fffb64688e6ea844229f711e5f11
louis <ws.choi@anypointmedia.com>
2023-12-08 10:31:46 +0900

    WithTimeout.kt

---
+++ src/main/kotlin/WithTimeout.kt
@@ -0,0 +1,12 @@
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+
+    withTimeout(1300L) {
+        repeat(1000) { i ->
+            println("I'm sleeping $i ...")
+            delay(500L)
+        }
+    }
+
+}
